5091c7c4a90a11c5e0bdabf5cc09edfdaa281a98
Tiago Oliveira <tiagolivesan@outlook.com>
2021-07-16 10:17:26 -0300

    Add Automatic button

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function Ligar() {
 function Apagar() {
   document.getElementById('luz').src = '/image/Loff.png'
 }
+function Blink() {
+  var intervalo = 0
+  var contador = 0
+
+  while (contador < 10) {
+    intervalo += 300;
+    setTimeout("document.getElementById('luz').src='/image/Lon.png'", intervalo)
+    intervalo += 300;
+    setTimeout("document.getElementById('luz').src='/image/Loff.png'", intervalo)
+    contador++;
+  }
+}
