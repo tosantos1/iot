9956c2cbd55e3831773fd93e9d4458af3e5ffe98
Academia de Robotica <tiagolivesan@outlook.com>
2021-09-14 18:53:58 -0300

    node js 'Error jade'

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var t = 'Express'
+  if (req.query.title){
+    t = req.query.title;
+  }
   res.render('index', { title: 'Express' });
 });
 
